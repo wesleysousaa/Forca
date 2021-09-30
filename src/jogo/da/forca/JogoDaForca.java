0de08f68717dfe23b02b599280b692d5380ae10e
5nyght <siginimitsig@gmail.com>
2021-09-30 14:54:57 -0300

    joguinho da forca
    ultima edição do dia 30/09

--- src/jogo/da/forca/JogoDaForca.java
+++ src/jogo/da/forca/JogoDaForca.java
@@ -1,8 +1,72 @@
 
 package jogo.da.forca;
+
+import java.util.Scanner;
+
 public class JogoDaForca {
     public static void main(String[] args) {
+        JogoForca novoJogo = new JogoForca();
+        Palavra palavra = new Palavra();
+        Scanner leitor = new Scanner(System.in);
+        String []dicas = new String[5];
+        String []dicionario = new String[5];
+        int vidas = 3;
+        int numPalavras = 0;
+        System.out.println("Digite 5 palavras");
+        int h = 0;
+        int k = 0;
+        for(int i = 0;i<5;i++){
+            h++;
+            System.out.println("Digite a "+h+"º palavra");
+            palavra.setPalavra(leitor.nextLine());
+            dicionario[i] = palavra.getPalavra();
+            System.out.println("Digite a dica dessa palavra: ");
+            palavra.setDica(leitor.nextLine());
+            dicas[i] = palavra.getDica();
+            
+        }
+        novoJogo.setDicionario(dicionario);
         
+        for(int i = 0;i<5 && vidas > 0;i++){
+            k++;
+            vidas = 3;
+            novoJogo.sortear();
+            int dica = 1;
+            int resposta = 0;
+            System.out.println("---------- PALAVRA "+k+" ----------");
+            System.out.println("VOCÊ TERÁ 3 VIDAS PARA CADA PALAVRA");
+            
+            for(int j = 0;0<vidas && novoJogo.testaSeAcabou() == false;j++){
+                System.out.println("VIDAS: "+vidas);
+                System.out.println("DICA DA PALAVRA:"+dicas[novoJogo.getPosicaoSorteada()]);
+                System.out.println("PALAVRA: "+novoJogo.getGabarito());
+                
+                if(dica == 1){
+                    System.out.println("GOSTARIA DE PEDIR UMA DICA? VOCÊ SÓ TEM CHANCE A 1 DICA POR PALAVRA\n1- SIM\n2- NÃO");
+                    resposta = Integer.parseInt(leitor.next());
+                    
+                }
+                if(resposta == 1 && dica == 1){
+                    novoJogo.pegarDica();
+                    dica = 0;
+                    
+                }
+                System.out.println("DIGITE UMA LETRA: ");
+                String letra = leitor.next();
+                char letraa = letra.charAt(0);
+                if(novoJogo.testarLetra(letraa) == false){
+                    System.out.println("NÃO HÁ ESTA LETRA NA PALAVRA");
+                    vidas-=1;
+                }
+                if(novoJogo.testaSeAcabou() && vidas>0){
+                    numPalavras +=1;
+                    break;
+                }
+            }
+            
+            
+        }
+        System.out.println("---------- FIM DE JOGO ----------");
     }
     
 }
--- src/jogo/da/forca/JogoForca.java
+++ src/jogo/da/forca/JogoForca.java
@@ -2,7 +2,7 @@ package jogo.da.forca;
 public class JogoForca {
     private String[] dicionario;
     private int posicaoSorteada;
-    private String gabarito;
+    private String gabarito = "";
 
     public int getPosicaoSorteada() {
         return posicaoSorteada;
@@ -20,28 +20,96 @@ public class JogoForca {
         this.dicionario = dicionario;
     }
     
-    
+    public String pegarDica(){
+       String []dica = gabarito.split("");
+       String palavra = dicionario[posicaoSorteada];
+       String pose = "?";
+       String pFinal = "";
+       int cont=0;
+       
+       for(int i = 0;i<=dica.length;i++){
+           cont += pose == dica[i]?1:0;
+           
+       }
+       int [] duvidas = new int[cont];
+       for(int i = 0;i<=dica.length;i++){
+           duvidas[i] = pose == dica[i]?i:-1;
+           
+       }
+       int p1 = duvidas[1];
+       int p2 = duvidas[1];
+       if(cont > 2){
+            for(int i = 0;p1==duvidas[i] ;i++){
+                p1 = (int)(Math.random()*gabarito.length());
+            }
+            for(int i = 0;p2==duvidas[i] ;i++){
+                p2 = (int)(Math.random()*gabarito.length());
+            }
+            dica[p1] = ""+palavra.charAt(p1);
+            dica[p2] = ""+palavra.charAt(p2);
+        }else if(cont == 2){
+            for(int i = 0;p1==duvidas[i] ;i++){
+                p1 = (int)(Math.random()*gabarito.length());
+            }
+            dica[p1] = ""+palavra.charAt(p1);
+        }else{
+            return "Só resta uma letra";
+        }
+       for(int i=0;i<=palavra.length();i++){
+           pFinal+=dica[i];
+       }
+       gabarito = pFinal;
+       return pFinal;
+    }
     public void sortear(){
         posicaoSorteada = (int)(Math.random()*dicionario.length);
         for(int i = 0;dicionario[posicaoSorteada]==null;i++){
             posicaoSorteada = (int)(Math.random()*dicionario.length);
         }
         String palavraS = dicionario[posicaoSorteada];
-        for(int i = 0;i<=palavraS.length();i++){
+        for(int i = 0;i<palavraS.length();i++){
             gabarito += "?";
         }
         
         
     }
     public boolean testarLetra(char letra){
-        String palavraS = ""+letra;
-        String letraa = dicionario[posicaoSorteada];
-        for(int i = 0;i<=dicionario[posicaoSorteada].length();i++){
-            
-            if(palavraS.charAt(i) == letraa.charAt(i)){
-                String letraaa = ""+palavraS.charAt(i);
-                gabarito.charAt(i) = letraaa;
+        int cont = 0;
+        String gabaritoNew ="";
+        if(gabarito.length()==0){
+            for(int i = 0;i<dicionario[posicaoSorteada].length();i++){
+                gabarito+="?";
             }
+        }
+        char [] novoGabarito = gabarito.toCharArray();
+        char [] palavraCerta = dicionario[posicaoSorteada].toCharArray();
+        for(int i = 0;i<gabarito.length();i++){
+            
+            if(letra == palavraCerta[i]){
+                novoGabarito[i] = letra;
+                cont++;
+            }else if(novoGabarito[i]=='?'){
+                novoGabarito[i] = '?';
+            }
+        }
+        for(int i = 0;i<gabarito.length();i++){
+            gabaritoNew += novoGabarito[i];        
+        }
+        gabarito = gabaritoNew;
+        return cont>0?true:false;
+    }
+    public boolean testaSeAcabou(){
+        int cont = 0;
+        for(int i = 0;i<gabarito.length();i++){
+            char l = gabarito.charAt(i);
+            cont += l == '?' ?1:0;
+        }
+        if(cont>0){
+            return false;
+        }else{
+            dicionario[posicaoSorteada] = null;
+            posicaoSorteada = -1;
+            return true;
         }
     }
 }
